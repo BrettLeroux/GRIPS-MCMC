9639aa51f95d2a6d0e44083d3336b467f4d1bf48
Michael J. Curry <curry@cs.umd.edu>
2020-07-10 17:15:55 -0400

    helium batch dim fix

--- qmc/wavefunction.py
+++ qmc/wavefunction.py
@@ -57,8 +57,8 @@ class HeliumTrialWavefunction(nn.Module):
     def forward(self, x):
         # outputs logprob
         # 2.0 * because it's |\Psi|^2
-       
-        return 2.0*(3*torch.log((2-self.alpha.clone().detach()))+torch.log(torch.tensor(np.pi))-(2-self.alpha)*(x[0]+x[1]))
+
+        return 2.0*(3*torch.log((2-self.alpha)+torch.log(torch.tensor(np.pi))-(2-self.alpha)*(x[:, 0]+x[:, 1])))
     
     def local_energy(self, x):
-        return -(2-self.alpha)**2-self.alpha*(1/x[0]+1/x[1])+1/(torch.sqrt(x[0]**2+x[1]**2+x[1]*x[0]*torch.cos(x[2]))) 
+        return -(2-self.alpha)**2-self.alpha*(1/x[0]+1/x[1])+1/(torch.sqrt(x[0]**2+x[1]**2+x[1]*x[0]*torch.cos(x[2])))
