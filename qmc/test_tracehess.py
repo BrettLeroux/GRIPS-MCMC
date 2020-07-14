ae40c884fe88c27f99e90eb0ce61d56ff7b50fad
Michael J. Curry <curry@cs.umd.edu>
2020-07-14 16:25:50 -0400

    added sin derivative test

--- qmc/test_tracehess.py
+++ qmc/test_tracehess.py
@@ -14,12 +14,21 @@ def func_quadratic(x):
 def func_cubic(x):
     return 10.0*torch.sum(x*x*x)
 
+def func_sin(x):
+    return torch.sin(x)
+
 
 # random arrays
 @given(arrays(np.float, 3, elements=floats(-10, 10)))
 def test_autograd_trace_hess_quadratic(x):
     assert autograd_trace_hessian(torch.tensor(x), func_quadratic) == 60.0
 
+
+@given(floats(-10, 10))
+def test_autograd_trace_hess_sin(x):
+    input = torch.tensor(x)
+    lap = autograd_trace_hessian(input, func_sin)
+    assert torch.isclose(lap, -torch.sin(input))
 
 def test_autograd_trace_cubic():
     assert autograd_trace_hessian(torch.ones(3), func_cubic) == 180.0
