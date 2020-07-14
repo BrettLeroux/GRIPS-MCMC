b45000a680f4b2035f17cccad20de77a26d6443c
Michael J. Curry <curry@cs.umd.edu>
2020-07-14 16:52:12 -0400

    add numerical tracehess tests

--- qmc/test_tracehess.py
+++ qmc/test_tracehess.py
@@ -4,7 +4,11 @@ from hypothesis import given
 from hypothesis.extra.numpy import arrays
 from hypothesis.strategies import floats
 
-from qmc.tracehess import autograd_trace_hessian
+from qmc.tracehess import autograd_trace_hessian, gradient_f, hessian_f
+
+# Gradient Function
+def the_func(y):
+    return y[0] ** 2 + y[1] ** 2
 
 
 def func_quadratic(x):
@@ -16,6 +20,18 @@ def func_cubic(x):
 
 def func_sin(x):
     return torch.sin(x)
+
+
+@given(arrays(np.float, (2, 1), elements=floats(-10, 10)))
+def test_numerical_gradient_f_nonan(x):
+    grad = gradient_f(x, the_func)
+    assert not np.isnan(grad).any()
+
+
+@given(arrays(np.float, (2, 1), elements=floats(-10, 10)))
+def test_numerical_hessian_f_nonan(x):
+    hess = hessian_f(x, the_func)
+    assert not np.isnan(hess).any()
 
 
 # random arrays
@@ -30,6 +46,6 @@ def test_autograd_trace_hess_sin(x):
     lap = autograd_trace_hessian(input, func_sin)
     assert torch.isclose(lap, -torch.sin(input))
 
+
 def test_autograd_trace_cubic():
     assert autograd_trace_hessian(torch.ones(3), func_cubic) == 180.0
-
