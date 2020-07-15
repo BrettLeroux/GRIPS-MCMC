e6237a75a4285579e75a6f025ea52dd036239055
Brett Leroux <beleroux@ucdavis.edu>
2020-07-15 15:52:37 -0700

    rosen2dim

--- qmc/distributions.py
+++ qmc/distributions.py
@@ -2,6 +2,32 @@ import torch
 import numpy as np
 from torch import nn
 
+                                                              
+
+class dim2Rosenbrock(nn.Module):
+    def __init__(self, alpha, beta):
+        super(dim2Rosenbrock, self).__init__()
+        self.alpha = nn.Parameter(alpha)
+        self.beta = nn.Parameter(beta)
+
+    def forward(self, x):
+        # output logprob
+        dim2 = x.ndimension() > 2
+        dim1 = x.ndimension() > 1 
+        if dim2:
+            result = - self.alpha * (x[:, :, 0] -1)**2 - self.beta * (x[:, :, 0]- x[:, :, 1]**2)**2
+        else:
+            x = x if dim1 else x.unsqueeze(0)
+            result = - self.alpha * (x[:, 0] -1)**2 - self.beta * (x[:, 0]- x[:, 1]**2)**2
+        return result if dim1 else result.squeeze(0)
+        
+
+    # gives the normalization constant
+    def normalization(self):
+        return torch.sqrt(self.alpha * self.beta) / np.pi
+
+
+
 
 class RandomHybridRosenbrock(nn.Module):
     # RandomHybridRosenbrock(m,n) creates the "Hybrid Rosenbrock" from https://arxiv.org/pdf/1903.09556.pdf with random parameters
--- qmc/test_distributions.py
+++ qmc/test_distributions.py
@@ -1,13 +1,51 @@
 from qmc.distributions import RandomHybridRosenbrock
+from qmc.distributions import dim2Rosenbrock
 import torch
 import numpy as np
 from hypothesis import given
 from hypothesis.extra.numpy import arrays
 from hypothesis.strategies import floats
 
+
+def test_dim2rosenbrock_logprob_dims():
+    config_dimension = 2
+    f = dim2Rosenbrock(torch.tensor(1/20), torch.tensor(100/20))
+
+    input = 0.5*torch.ones(10, config_dimension)
+    output = f(input)
+    assert len(output.shape) == 1
+    assert output.shape[0] == 10
+
+    input = 0.5*torch.ones(1, config_dimension)
+    output = f(input)
+    assert len(output.shape) == 1
+    assert output.shape[0] == 1
+
+    # for multiple iterations of multiple walkers, output should be one scalar per walker and iteration
+    input = 0.5*torch.ones(5, 10, config_dimension)
+    output = f(input)
+    assert len(output.shape) == 2
+    assert output.shape[0] == 5
+    assert output.shape[1] == 10
+    # input = 0.5*torch.ones(1, config_dimension)
+    # output = f(input)
+    # assert output.shape[0] == 1
+    #
+    # # for multiple iterations of multiple walkers, output should be one scalar per walker and iteration
+    # input = 0.5*torch.ones(5, 10, config_dimension)
+    # output = f(input)
+    # assert output.shape[0] == 5
+    # assert output.shape[1] == 10
+    #
+
+
+
+
+
+
 def test_rosenbrock_logprob_dims():
     config_dimension = 1
-    f = RandomHybridRosenbrock(1, 1)
+    f = RandomHybridRosenbrock(2, 1)
 
     input = 0.5*torch.ones(10, config_dimension)
     output = f(input)
