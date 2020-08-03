a180c60ecda61fb6129e7528ff77384e06ac3ba2
Michael J. Curry <curry@cs.umd.edu>
2020-08-03 16:32:23 -0400

    rename variables

--- qmc/local_energy.py
+++ qmc/local_energy.py
@@ -7,34 +7,34 @@ from qmc.wavefunction import HeliumTrialWavefunction
 from qmc.tracehess import autograd_trace_hessian, gradient_f, hessian_f
 
 def auto_hamiltonian_generator_atoms(ansatz, N_bodies, config):  #Unitless for now.
-    Kinetic_source = autograd_trace_hessian( ansatz,config, return_grad =True)
-    Kinetic_energy_0 = -0.5*Kinetic_source[0]
-    Kinetic_energy_1 = 0
+    kinetic_source = autograd_trace_hessian( ansatz,config, return_grad =True)
+    kinetic_energy_0 = -0.5*kinetic_source[0]
+    kinetic_energy_1 = 0
     for i in range(N_bodies):
-        r = (Kinetic_source[1][..., i])/config[...,i]
-        Kinetic_energy_1 = Kinetic_energy_1-r
-    Kinetic_total = Kinetic_energy_0 + Kinetic_energy_1
+        r = (kinetic_source[1][..., i])/config[...,i]
+        kinetic_energy_1 = kinetic_energy_1-r
+    kinetic_total = kinetic_energy_0 + kinetic_energy_1
 
-    Potential_nucleous_electron = 0
+    potential_nucleous_electron = 0
     for i in range(N_bodies):
-        Potential_nucleous_electron = Potential_nucleous_electron-(N_bodies/(config[...,i].squeeze(dim=-1)))*ansatz(config)
-    Potential_electron_electron = 0
+        potential_nucleous_electron = potential_nucleous_electron-(N_bodies/(config[...,i].squeeze(dim=-1)))*ansatz(config)
+    potential_electron_electron = 0
     k = 1
     for i in range(N_bodies):
         for j in range(i+1,N_bodies):
-            Potential_electron_electron = Potential_electron_electron + (ansatz(config))/(torch.sqrt(config[...,i]**2+config[...,j]**2+torch.abs(config[...,i])*torch.abs(config[...,j])*torch.cos(config[...,N_bodies -1+k])))
+            potential_electron_electron = potential_electron_electron + (ansatz(config))/(torch.sqrt(config[...,i]**2+config[...,j]**2+torch.abs(config[...,i])*torch.abs(config[...,j])*torch.cos(config[...,N_bodies -1+k])))
             k+=1
-    Energy_total = Kinetic_total+Potential_nucleous_electron+Potential_electron_electron
-    return Energy_total
+    energy_total = kinetic_total+potential_nucleous_electron+potential_electron_electron
+    return energy_total
    
 tf_0=HeliumTrialWavefunction(torch.ones(1)*1.8)
 tf_1=HydrogenTrialWavefunction(torch.ones(2))
 
-def Local_energy_of_psi_at_x( ansatz, N_bodies, config):
-    Local_energy_at_x = auto_hamiltonian_generator_atoms(ansatz, N_bodies, config)/ansatz(config)
+def local_energy_of_psi_at_x(ansatz, N_bodies, config):
+    local_energy_at_x = auto_hamiltonian_generator_atoms(ansatz, N_bodies, config)/ansatz(config)
     
     
-    return Local_energy_at_x
+    return local_energy_at_x
 
-print(Local_energy_of_psi_at_x(tf_0, 2, torch.rand(10,4, 3)))
+print(local_energy_of_psi_at_x(tf_0, 2, torch.rand(10, 4, 3)))
 
