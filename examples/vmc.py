39aaacd518b63625364b1413330065ed59b0ccc5
Michael J. Curry <curry@cs.umd.edu>
2020-08-06 17:10:40 -0400

    save hessian code here (doesn't seem to be good)

--- examples/vmc.py
+++ examples/vmc.py
@@ -18,6 +18,36 @@ def energy_minimize_step(trialfunc, samples, optimizer):
     optimizer.step()
     print(init_config)
 
+def energy_minimize_explicit_grads(trialfunc, samples, step_size=5e-2):
+    local_energies = trialfunc.local_energy(samples).detach()
+    mean_local_energy = local_energies.mean()
+    sample_logprobs = trialfunc(samples)
+    loss = ((local_energies - mean_local_energy) * sample_logprobs).mean()
+    grads_wrt_parameters  = torch.autograd.grad(loss, trialfunc.parameters())
+    with torch.no_grad():
+        for (param, grad) in zip(trialfunc.parameters(), grads_wrt_parameters):
+            param -= step_size*grad
+
+def full_hessian(loss_grad_vector, parameters):
+    all_rows = []
+    for i in range(loss_grad_vector.shape[-1]):
+        hessian_row, = torch.autograd.grad(loss_grad_vector[..., i], parameters, retain_graph=True)
+        all_rows.append(hessian_row)
+    return torch.stack(all_rows)
+
+def energy_minimize_newton(trialfunc, samples, optimizer, step_size=5e-2):
+    local_energies = trialfunc.local_energy(samples).detach()
+    mean_local_energy = local_energies.mean()
+    print('energy is ', mean_local_energy)
+    sample_logprobs = trialfunc(samples)
+    loss = ((local_energies - mean_local_energy) * sample_logprobs).mean()
+    grads_wrt_parameters = torch.autograd.grad(loss, trialfunc.parameters(), create_graph=True)
+    hessians_wrt_parameters = [full_hessian(grad, param) for grad, param in zip(grads_wrt_parameters, trialfunc.parameters())]
+    with torch.no_grad():
+        for (param, grad, hessian) in zip(trialfunc.parameters(), grads_wrt_parameters, hessians_wrt_parameters):
+            param -= step_size*(torch.inverse(hessian) @ grad)
+
+
 
 
 def vmc_iterate(tf, init_config, num_iters=100):
@@ -25,7 +55,7 @@ def vmc_iterate(tf, init_config, num_iters=100):
     # propdist = NormalProposal(0.3)
     propdist = ClipNormalProposal(0.1)
     for i in range(num_iters):
-        results=metropolis_symmetric(tf, init_config, propdist, num_walkers=100, num_steps=50000)
+        results=metropolis_symmetric(tf, init_config, propdist, num_walkers=100, num_steps=10000)
         energy_minimize_step(tf, results, opt)
         print(tf.alpha)
         print(tf.alpha[...,0])
@@ -67,13 +97,14 @@ def helium_energy_alpha_values():
     return vals, means
 
 if __name__ == '__main__':
-    tf = twoinone(torch.tensor([3.0,2.5]))
-    init_config = 0.5*torch.rand(2)
-    vmc_iterate(tf, init_config)
+    # tf = twoinone(torch.tensor([5.0,6.0]))
+    # init_config = 0.5*torch.rand(2)
+    # vmc_iterate(tf, init_config)
     # helium_energy_alpha_values()
 #if __name__ == '__main__':
-#    tf = HeliumTrialWavefunction(torch.ones(1))
- #   init_config = 0.5*torch.ones(1000,3)
- #   vmc_iterate(tf, init_config)
+    tf = HydrogenTrialWavefunction(torch.tensor([1.0]))
+    init_config = 0.5*torch.ones(1000, 1)
+    vmc_iterate(tf, init_config)
+    print(tf.alpha)
     # helium_energy_alpha_values()
 
